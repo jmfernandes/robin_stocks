1b3523503e9a41a39723c29e0d95132d2ed5296f
jmfernandes <joshfernandes@mac.com>
2018-02-22 20:51:41 -0800

    added long description

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 from setuptools import setup
 
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name='robin_stocks',
       version='0.1',
       description='Made to interact with the Robinhood API',
+      long_description=long_description,
       url='https://github.com/jmfernandes/Robinhood',
       author='Josh Fernandes',
       author_email='joshfernandes@mac.com',
