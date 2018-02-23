0314dcdabbbf886c622f3f2d9304d158f88cbc7b
jmfernandes <joshfernandes@mac.com>
2018-02-22 16:50:11 -0800

    init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='robinhood',
+      version='0.1',
+      description='Made to interact with the Robinhood API',
+      url='http://github.com/storborg/funniest',
+      author='Josh Fernandes',
+      author_email='joshfernandes@mac.com',
+      license='MIT',
+      packages=['robinhood'],
+      zip_safe=False)
