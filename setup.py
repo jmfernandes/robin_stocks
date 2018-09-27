bf2f95438ce61b24155c313b97314095d5a5145d
jmfernandes <joshfernandes@mac.com>
2018-09-26 21:20:49 -0700

    changed readme to txt

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.txt'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='robin_stocks',
