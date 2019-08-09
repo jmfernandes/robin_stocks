ca12070487aea7b1e2a2bb81e598ae262e0caebc
jmfernandes <joshfernandes@mac.com>
2019-08-08 21:56:12 -0700

    update version in setup

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='robin_stocks',
-      version='0.9.8.7',
+      version='0.9.8.8',
       description='A Python wrapper around the Robinhood API',
       long_description=long_description,
       url='https://github.com/jmfernandes/robin_stocks',
