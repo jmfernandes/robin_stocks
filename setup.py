c449aabb3816946f88976b0e6991ee1c2a09f3ab
jmfernandes <joshfernandes@mac.com>
2019-09-01 17:05:02 -0700

    0.9.9.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='robin_stocks',
-      version='0.9.9.0',
+      version='0.9.9.1',
       description='A Python wrapper around the Robinhood API',
       long_description=long_description,
       long_description_content_type='text/markdown',
