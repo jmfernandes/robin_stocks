03ffda8f2e15cd761883d57d9cfb7a79bbdc332d
Joshua M. Fernandes <joshfernandes@mac.com>
2020-01-28 11:27:29 -0800

    changed spacing

--- test_robin.py
+++ test_robin.py
@@ -1,4 +1,3 @@
-
 import os
 import robin-stocks as r
 
@@ -13,18 +12,7 @@ class TestLogin:
         print('logging out')
         r.logout()
         
-     def test_one():
+    def test_one():
         profile_info = r.load_account_profile()
         print(profile_info)
         assert profile_info
-        
-        
-        
-# # content of test_robin.py
-# def func(x):
-#     return x + 1
-
-# def test_answer():
-#     assert func(3) == 4
-
-# #    assert os.environ['robin_username'] == "blah"
