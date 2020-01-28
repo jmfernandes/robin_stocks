709ea09d7428f86268d1828251938f4da3dc7003
Joshua M. Fernandes <joshfernandes@mac.com>
2020-01-28 11:32:35 -0800

    removed login test

--- test_robin.py
+++ test_robin.py
@@ -1,18 +1,25 @@
 import os
 import robin_stocks as r
 
-class TestLogin:
-    @classmethod
-    def setup_class(cls):
-        print('setting up')
-        r.login(os.environ['robin_username'], os.environ['robin_password'])
-     
-    @classmethod
-    def teardown_class(cls):
-        print('logging out')
-        r.logout()
-        
-    def test_one():
-        profile_info = r.load_account_profile()
+class TestStocks:
+    def test_quotes():
+        profile_info = r.get_quotes('spy')
         print(profile_info)
         assert profile_info
+
+        
+# class TestLogin:
+#     @classmethod
+#     def setup_class(cls):
+#         print('setting up')
+#         r.login(os.environ['robin_username'], os.environ['robin_password'])
+     
+#     @classmethod
+#     def teardown_class(cls):
+#         print('logging out')
+#         r.logout()
+        
+#     def test_one():
+#         profile_info = r.load_account_profile()
+#         print(profile_info)
+#         assert profile_info
