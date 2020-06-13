12b04266f99ee24a1479c777f37f7517ac32de5c
josh fernandes <joshfernandes@mac.com>
2020-06-13 15:03:03 -0700

    added more tests to test quotes

--- tests/test_automated.py
+++ tests/test_automated.py
@@ -47,10 +47,33 @@ class TestStocks(unittest.TestCase):
     def setUp(self):
         self.single_stock = 'AAPL'
         self.list_stocks = ['tsla', 'f', 'plug', 'fB', 'SPY', 'botz', 'jnug']
+        self.fake_stocks = ['87627273', 'ffffffffff']
 
     def test_quotes(self):
-        quote = r.get_quotes(self.single_stock, info=None)
-        self.assertEqual(quote[0]['symbol'], self.single_stock)
+        quote = r.get_quotes(self.single_stock, info=None)[0]
+        self.assertEqual(quote['symbol'], self.single_stock)
+        self.assertIn('ask_price', quote)
+        self.assertIn('ask_size', quote)
+        self.assertIn('bid_price', quote)
+        self.assertIn('bid_size', quote)
+        self.assertIn('last_trade_price', quote)
+        self.assertIn('last_extended_hours_trade_price', quote)
+        self.assertIn('previous_close', quote)
+        self.assertIn('adjusted_previous_close', quote)
+        self.assertIn('previous_close_date', quote)
+        self.assertIn('symbol', quote)
+        self.assertIn('trading_halted', quote)
+        self.assertIn('has_traded', quote)
+        self.assertIn('last_trade_price_source', quote)
+        self.assertIn('updated_at', quote)
+        self.assertIn('instrument', quote)
+        #
+        more_quotes = r.get_quotes(self.list_stocks, info=None)
+        self.assertEqual(len(more_quotes), len(self.list_stocks))
+        #
+        fake_quotes = r.get_quotes(self.fake_stocks, info=None)
+        self.assertEqual(len(fake_quotes), 1)
+        self.assertEqual(fake_quotes[0], None)
 
 
 if __name__ == '__main__':
