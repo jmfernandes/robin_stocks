493353d3aa73a809d92b274fb9c115e46c076cc3
josh fernandes <joshfernandes@mac.com>
2020-07-21 21:31:58 -0700

    added automated login to action tests

--- tests/test_github_actions.py
+++ tests/test_github_actions.py
@@ -1,6 +1,7 @@
 # Used by git Actions
 import os
 import robin_stocks as r
+import pyotp
 
 class TestStocks:
     def test_quotes(self):
@@ -10,3 +11,17 @@ class TestStocks:
     def test_name_apple(self):
         name = r.get_name_by_symbol('aapl')
         assert name == "Apple"
+
+class TestProfiles:
+    @classmethod
+    def setup_class(cls):
+        totp  = pyotp.TOTP(robin_mfa).now()
+        login = r.login(robin_username, robin_password, mfa_code=totp)
+
+    @classmethod
+    def teardown_class(cls):
+        r.logout()
+
+    def test_load_account_profile(self):
+        info = r.load_account_profile()
+        assert info
