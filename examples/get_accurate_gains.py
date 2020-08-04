8120a8f3221bf2ff07e3f5e63c81ced2e36e8659
Sachin Paryani <sachin.paryani@gmail.com>
2020-08-03 17:38:33 -0700

    Include 'fees' in dividend calculation in example
    In the example get_accurate_gains.py, the dividend calculation does not take into consideration the fees that you pay robinhood when there is insufficient balance in your bank account and you still try to deposit funds.

--- examples/get_accurate_gains.py
+++ examples/get_accurate_gains.py
@@ -18,21 +18,23 @@ password = ''
 login = r.login(username,password)
 
 profileData = r.load_portfolio_profile()
-#print(profileData)
 allTransactions = r.get_bank_transfers()
 cardTransactions= r.get_card_transactions()
 
 deposits = sum(float(x['amount']) for x in allTransactions if (x['direction'] == 'deposit') and (x['state'] == 'completed'))
 withdrawals = sum(float(x['amount']) for x in allTransactions if (x['direction'] == 'withdraw') and (x['state'] == 'completed'))
 debits = sum(float(x['amount']['amount']) for x in cardTransactions if (x['direction'] == 'debit' and (x['transaction_type'] == 'settled')))
-money_invested = deposits - (withdrawals - debits)
+reversal_fees = sum(float(x['fees']) for x in allTransactions if (x['direction'] == 'deposit') and (x['state'] == 'reversed'))
 
+money_invested = deposits + reversal_fees - (withdrawals - debits)
 dividends = r.get_total_dividends()
 percentDividend = dividends/money_invested*100
 
-totalGainMinusDividends =float(profileData['extended_hours_equity'])-dividends-money_invested
+equity = float(profileData['extended_hours_equity'])
+totalGainMinusDividends = equity - dividends - money_invested
 percentGain = totalGainMinusDividends/money_invested*100
 
-print("The total money invested is {}".format(money_invested))
-print("The net worth has increased {:0.2}% due to dividends".format(percentDividend))
-print("The net worth has increased {:0.3}% due to other gains".format(percentGain))
+print("The total money invested is {:.2f}".format(money_invested))
+print("The total equity is {:.2f}".format(equity))
+print("The net worth has increased {:0.2}% due to dividends that amount to {:0.2f}".format(percentDividend, dividends))
+print("The net worth has increased {:0.3}% due to other gains that amount to {:0.2f}".format(percentGain, totalGainMinusDividends))
