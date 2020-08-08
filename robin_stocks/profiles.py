57cb64b15686409f8ac8682b805ce1003536171a
josh fernandes <joshfernandes@mac.com>
2020-08-08 10:40:46 -0700

    added profile tests and documentation

--- robin_stocks/profiles.py
+++ robin_stocks/profiles.py
@@ -13,6 +13,51 @@ def load_account_profile(info=None):
     :returns: The function returns a dictionary of key/value pairs. \
     If a string is passed in to the info parameter, then the function will return \
     a string corresponding to the value of the key whose name matches the info parameter.
+    :Dictionary Keys: * url
+                      * portfolio_cash
+                      * can_downgrade_to_cash
+                      * user
+                      * account_number
+                      * type
+                      * created_at
+                      * updated_at
+                      * deactivated
+                      * deposit_halted
+                      * only_position_closing_trades
+                      * buying_power
+                      * cash_available_for_withdrawal
+                      * cash
+                      * cash_held_for_orders
+                      * uncleared_deposits
+                      * sma
+                      * sma_held_for_orders
+                      * unsettled_funds
+                      * unsettled_debit
+                      * crypto_buying_power
+                      * max_ach_early_access_amount
+                      * cash_balances
+                      * margin_balances
+                      * sweep_enabled
+                      * instant_eligibility
+                      * option_level
+                      * is_pinnacle_account
+                      * rhs_account_number
+                      * state
+                      * active_subscription_id
+                      * locked
+                      * permanently_deactivated
+                      * received_ach_debit_locked
+                      * drip_enabled
+                      * eligible_for_fractionals
+                      * eligible_for_drip
+                      * eligible_for_cash_management
+                      * eligible_for_trading_on_expiration
+                      * cash_management_enabled
+                      * option_trading_on_expiration_enabled
+                      * cash_held_for_options_collateral
+                      * fractional_position_closing_only
+                      * user_id
+                      * rhs_stock_loan_consent_status
 
     """
     url = urls.account_profile()
@@ -30,6 +75,20 @@ def load_basic_profile(info=None):
     :returns: The function returns a dictionary of key/value pairs. If a string \
     is passed in to the info parameter, then the function will return a string \
     corresponding to the value of the key whose name matches the info parameter.
+    :Dictionary Keys: * user
+                      * address
+                      * city
+                      * state
+                      * zipcode
+                      * phone_number
+                      * marital_status
+                      * date_of_birth
+                      * citizenship
+                      * country_of_residence
+                      * number_dependents
+                      * signup_as_rhs
+                      * tax_id_ssn
+                      * updated_at
 
     """
     url = urls.basic_profile()
@@ -47,6 +106,24 @@ def load_investment_profile(info=None):
     :returns: The function returns a dictionary of key/value pairs. \
     If a string is passed in to the info parameter, then the function will return \
     a string corresponding to the value of the key whose name matches the info parameter.
+    :Dictionary Keys: * user
+                      * total_net_worth
+                      * annual_income
+                      * source_of_funds
+                      * investment_objective
+                      * investment_experience
+                      * liquid_net_worth
+                      * risk_tolerance
+                      * tax_bracket
+                      * time_horizon
+                      * liquidity_needs
+                      * investment_experience_collected
+                      * suitability_verified
+                      * option_trading_experience
+                      * professional_trader
+                      * understand_option_spreads
+                      * interested_in_options
+                      * updated_at
 
     """
     url = urls.investment_profile()
@@ -64,6 +141,28 @@ def load_portfolio_profile(info=None):
     :returns: The function returns a dictionary of key/value pairs. \
     If a string is passed in to the info parameter, then the function will return \
     a string corresponding to the value of the key whose name matches the info parameter.
+    :Dictionary Keys: * url
+                      * account
+                      * start_date
+                      * market_value
+                      * equity
+                      * extended_hours_market_value
+                      * extended_hours_equity
+                      * extended_hours_portfolio_equity
+                      * last_core_market_value
+                      * last_core_equity
+                      * last_core_portfolio_equity
+                      * excess_margin
+                      * excess_maintenance
+                      * excess_margin_with_uncleared_deposits
+                      * excess_maintenance_with_uncleared_deposits
+                      * equity_previous_close
+                      * portfolio_equity_previous_close
+                      * adjusted_equity_previous_close
+                      * adjusted_portfolio_equity_previous_close
+                      * withdrawable_amount
+                      * unwithdrawable_deposits
+                      * unwithdrawable_grants
 
     """
     url = urls.portfolio_profile()
@@ -80,6 +179,23 @@ def load_security_profile(info=None):
     :returns: The function returns a dictionary of key/value pairs. \
     If a string is passed in to the info parameter, then the function will return \
     a string corresponding to the value of the key whose name matches the info parameter.
+    :Dictionary Keys: * user
+                      * object_to_disclosure
+                      * sweep_consent
+                      * control_person
+                      * control_person_security_symbol
+                      * security_affiliated_employee
+                      * security_affiliated_firm_relationship
+                      * security_affiliated_firm_name
+                      * security_affiliated_person_name
+                      * security_affiliated_address
+                      * security_affiliated_address_subject
+                      * security_affiliated_requires_duplicates
+                      * stock_loan_consent_status
+                      * agreed_to_rhs
+                      * agreed_to_rhs_margin
+                      * rhs_stock_loan_consent_status
+                      * updated_at
 
     """
     url = urls.security_profile()
@@ -97,6 +213,17 @@ def load_user_profile(info=None):
     :returns: The function returns a dictionary of key/value pairs. \
     If a string is passed in to the info parameter, then the function will return \
     a string corresponding to the value of the key whose name matches the info parameter.
+    :Dictionary Keys: * url
+                      * id
+                      * id_info
+                      * username
+                      * email
+                      * email_verified
+                      * first_name
+                      * last_name
+                      * origin
+                      * profile_name
+                      * created_at
 
     """
     url = urls.user_profile()
