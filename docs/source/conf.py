868a362b500c6e39c7c8af01a84a4099244a7b76
josh fernandes <joshfernandes@mac.com>
2023-07-05 20:09:30 -0700

    updated documentation file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ import sys
 sys.path.insert(0, os.path.abspath('../../'))
 
 def setup(app):
-    app.add_stylesheet('style.css')
+    app.add_css_file('style.css')
 
 
 # -- General configuration ------------------------------------------------
