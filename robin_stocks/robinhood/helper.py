d0a904269c78bb32940db5ed741fa413efb5fbc0
Brad <bvandorf@gmail.com>
2023-08-01 17:12:18 -0500

    found simpler way to catch exception that is happening before the overall exception

--- robin_stocks/robinhood/helper.py
+++ robin_stocks/robinhood/helper.py
@@ -350,16 +350,13 @@ def request_post(url, payload=None, timeout=16, json=False, jsonify_data=True):
         else:
             res = SESSION.post(url, data=payload, timeout=timeout)
         if res.status_code == 400:
-            print("Error in request_post: {0}".format(message), file=get_output())
+            print("Error in request_post status_code 400", file=get_output())
         else:
             raise Exception("Received "+ str(res.status_code))
         data = res.json()
     except Exception as message:
         # Either return response <200,401,etc.> or the data that is returned from requests.
         print("Error in request_post: {0}".format(message), file=get_output())
-        if (res.status_code != 400):
-            raise Exception("Received "+ str(res.status_code))
-        data = res.json()
     if jsonify_data:
         return(data)
     else:
