85a7622c2c34bd3978644df45ca6b966b5f83e63
Brad <bvandorf@gmail.com>
2023-08-06 15:57:19 -0500

    Update helper.py

--- robin_stocks/robinhood/helper.py
+++ robin_stocks/robinhood/helper.py
@@ -349,13 +349,10 @@ def request_post(url, payload=None, timeout=16, json=False, jsonify_data=True):
                 'Content-Type', 'application/x-www-form-urlencoded; charset=utf-8')
         else:
             res = SESSION.post(url, data=payload, timeout=timeout)
-        if res.status_code == 400:
-            print("Error in request_post status_code 400", file=get_output())
-        else:
+        if res.status_code not in [200, 201, 202, 204, 301, 302, 303, 304, 307, 400, 401, 402, 403]:
             raise Exception("Received "+ str(res.status_code))
         data = res.json()
     except Exception as message:
-        # Either return response <200,401,etc.> or the data that is returned from requests.
         print("Error in request_post: {0}".format(message), file=get_output())
     if jsonify_data:
         return(data)
