474db23f15dbfcd5b6a52be20b546551904d3a93
Anarghya Das <anarghya@buffalo.edu>
2024-07-05 17:21:25 -0400

    added interest payment function

--- robin_stocks/robinhood/account.py
+++ robin_stocks/robinhood/account.py
@@ -440,6 +440,19 @@ def get_stock_loan_payments(info=None):
     data = request_get(url, 'pagination')
     return(filter_data(data, info))
 
+@login_required
+def get_interest_payments(info=None):
+    """Returns a list of interest payments.
+
+    :param info: Will filter the results to get a specific value.
+    :type info: Optional[str]
+    :returns: Returns a list of dictionaries of key/value pairs for each interest payment. If info parameter is provided, \
+    a list of strings is returned where the strings are the value of the key that matches info.
+
+    """
+    url = interest_url()
+    data = request_get(url, 'pagination')
+    return(filter_data(data, info))
 
 @login_required
 def get_margin_interest(info=None):
