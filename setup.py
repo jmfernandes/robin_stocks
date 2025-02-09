ad893ad4a1a2798ae0f08393715783ae4df01a43
sully <ssmanji89@gmail.com>
2025-02-09 11:47:04 -0600

    Bugfix: Fix KeyError in challenge response and update fallback handling for pickle file loading; update version to 3.0.6. (Based on work by Gates8911)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='robin_stocks',
-      version='3.0.5',
+      version='3.0.6',
       description='A Python wrapper around the Robinhood API',
       long_description=long_description,
       long_description_content_type='text/x-rst',
